036d1b624c9b4d979a8e99ce631e22b4b818545d
Emre Erkoca <emreerkoca@outlook.com>
2021-11-27 14:48:28 +0300

    Angular and Api app removed. Api project added

---
+++ LearnAndRepeatWeb/LearnAndRepeatWeb.Api/Program.cs
@@ -0,0 +1,29 @@
+using LearnAndRepeatWeb.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnAndRepeatWeb.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FluentMigratorConfigurator.MigrateUp();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ LearnAndRepeatWeb/LearnAndRepeatWeb.Infrastructure/Data/Migrations/Migration_0002_CreateTable_Sample.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnAndRepeatWeb.Infrastructure.Data.Migrations
+{
+    [Migration(2)]
+    public class Migration_0002_CreateTable_Sample : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Sample");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Sample")
+                .WithColumn("Id").AsInt64().PrimaryKey().NotNullable()
+                .WithColumn("FirstName").AsString().NotNullable()
+                .WithColumn("LastName").AsString().NotNullable()
+                .WithColumn("Email").AsString().NotNullable()
+                .WithColumn("Password").AsString().NotNullable()
+                .WithColumn("CreateDate").AsDateTime2().NotNullable()
+                .WithColumn("UpdateDate").AsDateTime2().NotNullable();
+        }
+    }
+}
