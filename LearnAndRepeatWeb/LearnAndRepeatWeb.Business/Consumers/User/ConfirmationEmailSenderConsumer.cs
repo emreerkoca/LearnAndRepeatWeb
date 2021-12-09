adfec51040216d8bd157e069ad4fc826bc8b7fa0
Emre Erkoca <emreerkoca@outlook.com>
2021-12-10 01:28:32 +0300

    MassTransit configuration added for RabbitMQ UserCreated event published ConfirmationEmailSenderConsumer created

---
+++ LearnAndRepeatWeb/LearnAndRepeatWeb.Business/Consumers/User/ConfirmationEmailSenderConsumer.cs
@@ -0,0 +1,14 @@
+﻿using LearnAndRepeatWeb.Contracts.Events.User;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace LearnAndRepeatWeb.Business.Consumers.User
+{
+    public class ConfirmationEmailSenderConsumer : IConsumer<UserCreatedEvent>
+    {
+        public async Task Consume(ConsumeContext<UserCreatedEvent> context)
+        {
+            UserCreatedEvent userCreatedEvent = context.Message;
+        }
+    }
+}
