f623bbe4d7d83154fc70ee999cfee6a543d75ee5
Emre Erkoca <emreerkoca@outlook.com>
2022-03-03 17:23:45 +0300

    health-check endpoint added

---
+++ LearnAndRepeatWeb/LearnAndRepeatWeb.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+
+namespace LearnAndRepeatWeb.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("health-check")]
+        public IActionResult Get()
+        {
+            return StatusCode((int)HttpStatusCode.OK, Environment.MachineName);
+        }
+    }
+}
